433d9d1386fbb4c13faa9b7236587b05fcf31c47
judeanderson2031 <judeanderson07@gmail.com>
2025-09-12 13:59:48 +0000

    Class

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    system.out.println("Jude Anderson");
+
+    system.out.println("Jude Anderson \nBurger King");
+   
+    
+  }
+
+  
+}
